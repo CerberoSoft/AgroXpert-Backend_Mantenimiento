fa57f3ea605b8e4d8703d6204eb55fb8405055ec
Juan David Osorio <72204602+jd-oh@users.noreply.github.com>
2023-06-03 14:07:04 -0600

    Se agregaron comentarios para el método 'UpdateEstimatesHarvest'

--- src/repositories/harvest_repository.go
+++ src/repositories/harvest_repository.go
@@ -135,6 +135,19 @@ func UpdateSummaryFinalProduction(idHarvest string, idFinalProduction primitive.
 	return nil
 }
 
+
+// Actualiza la lista de las estimaciones de cosechas asociadas a una cosecha
+// específica. Esta función es necesaria para mantener un registro preciso y actualizado de las estimaciones 
+//realizadas a lo largo del tiempo. Al agregar nuevas, se permite un seguimiento 
+//más efectivo y una evaluación del rendimiento y las expectativas de la cosecha en cuestión.
+
+// @param idHarvest: Es el id de la cosecha a la que se desea agregar la nueva estimación. Con este se
+// buscará y actualizará la cosecha correspondiente en la base de datos.
+
+// @param idNewEstimate: Es el id de la nueva estimación que se desea agregar a la lista.
+
+// @return error: Un error que indica un fallo en la operación. Si no hay errores, se devuelve 'nil'.
+
 func UpdateEstimatesHarvest(idHarvest string, idNewEstimate primitive.ObjectID) error {
 	collection := database.Db.GetCollection("Harvest")
 
